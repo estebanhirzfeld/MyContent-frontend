1b185108b8a0cbd419ecaa4d985cd250a7e31fde
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-07-12 12:13:57 -0300

    feat: added vercel adapter + server output mode

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,5 +6,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'hybrid'
+  output: 'server',
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
